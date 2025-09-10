1648a9a936aa3d9725271d4a6e9bd21faa82a1cc
Edwino2400 <edwinchigara@gmail.com>
2025-09-10 14:24:51 +0200

    3rd exercise

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Parts 1 and 2 user entered number
+        Console.Write("What is the generated number?");
+        int generatedNumber = int.Parse(Console.ReadLine());
+        
+        // Part 3 random number
+        //Random randomGenerator = new Random();
+        //int generatedNumber = randomGenerator.Next(1, 101);
+
+        int guess = 9;
+
+        
+        while (guess != generatedNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (generatedNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (generatedNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations the answer was correct!");
+            }
+
+        }                    
     }
 }
