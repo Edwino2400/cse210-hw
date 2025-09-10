76fdb8a330c50d8c3712be896f5276ded9ca26b3
Edwino2400 <edwinchigara@gmail.com>
2025-09-10 14:47:37 +0200

    4th exercise

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,56 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        
+        
+        int enteredNumber = -1;
+        while (enteredNumber != 0)
+        {
+            Console.Write("Enter a number or enter 0 to quit: ");
+            
+            string userResponse = Console.ReadLine();
+            enteredNumber = int.Parse(userResponse);
+            
+            // Number not zero entered
+            if (enteredNumber != 0)
+            {
+                numbers.Add(enteredNumber);
+            }
+        }
+
+        // Part 1: Calculate sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Part 2: Calculate the average
+        
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Part 3: Find the Maximum
+       
+        
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
