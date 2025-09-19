e9ee3e12312d5ec1fe18c99b049fb93a61ad4a1e
Edwino2400 <edwinchigara@gmail.com>
2025-09-19 15:18:08 +0200

    Abstraction Assingment Submission

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,40 +1,24 @@
 using System;
-using System.Collections.Generic;
-
-
-
-
-public class Job
-{
-    public string _jobTitle;
-    public string _company;
-    public int _startYear;
-    public int _endYear;
-
-    public void Display()
-    {
-        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
-    }
-}
 
 class Program
 {
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Teacher";
-        job1._company = "St Patrick's School";
-        job1._startYear = 2011;
-        job1._endYear = 2013;
+        job1._jobTitle = "Miner";
+        job1._company = "Unki Mine";
+        job1._startYear = 2001;
+        job1._endYear = 2015;
 
         Job job2 = new Job();
-        job2._jobTitle = "Miner";
-        job2._company = "Anglo American";
+        job2._jobTitle = "Teacher";
+        job2._company = "Bata";
         job2._startYear = 2016;
-        job2._endYear = 2024;
+        job2._endYear = 2025;
 
         Resume myResume = new Resume();
-        myResume._name = "Edwin Chigara";
+        myResume._name = "Sean Timber";
+
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
 
