8305cf66e765f5be239a70d8f268642fad3b241a
Edwino2400 <edwinchigara@gmail.com>
2025-10-06 10:40:41 +0200

    Inheritance homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+       
+        Assignment a1 = new Assignment("Edwin Chigara", "Divison");
+        Console.WriteLine(a1.GetSummary());
+
+       
+        MathAssignment a2 = new MathAssignment("Peter Nyoni", "Fractions", "5.6", "1-10");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Sekai Chikosi", "African History", "Munhumutapa Empire");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
